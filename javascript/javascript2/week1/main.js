70710e11664fd6772d8f38277e9ce90963527e94
VioMNE <violeta.pavetic@gmail.com>
2022-07-08 21:09:20 +0200

    Formated document

--- javascript/javascript2/week1/main.js
+++ javascript/javascript2/week1/main.js
@@ -3,22 +3,22 @@ console.log("Script loaded");
 const products = getAvailableProducts();
 console.log(products);
 function renderProducts(products) {
-    const ulList = document.createElement("ul");
-    document.body.appendChild(ulList);
+  const ulList = document.createElement("ul");
+  document.body.appendChild(ulList);
 
-    for(let product of products) {
-        const liList = document.createElement("li");
-        ulList.appendChild(liList);
-        const h2product = document.createElement("h2");
-        h2product.innerHTML = product.name;
-        liList.appendChild(h2product);
-        const divPrice = document.createElement("div");
-        divPrice.innerHTML = `price: ${product.price}`;
-        liList.appendChild(divPrice);
-        const divRating = document.createElement("div");
-        divRating.innerHTML = `Rating: ${product.rating}`;
-        liList.appendChild(divRating);
-    }
+  for (let product of products) {
+    const liList = document.createElement("li");
+    ulList.appendChild(liList);
+    const h2product = document.createElement("h2");
+    h2product.innerHTML = product.name;
+    liList.appendChild(h2product);
+    const divPrice = document.createElement("div");
+    divPrice.innerHTML = `price: ${product.price}`;
+    liList.appendChild(divPrice);
+    const divRating = document.createElement("div");
+    divRating.innerHTML = `Rating: ${product.rating}`;
+    liList.appendChild(divRating);
+  }
 }
 
-renderProducts(products); 
+renderProducts(products);
--- javascript/javascript2/week1/spiritanimalg.js
+++ javascript/javascript2/week1/spiritanimalg.js
@@ -1,31 +1,25 @@
+const spiritAnimal = [
+  "Confident Bear",
+  "Competitive Wolf",
+  "Introverted Panda",
+  "Graceful Butterfly",
+  "Powerful Horse",
+  "Mischivous Fox",
+  "Strongwilled Tiger",
+  "Couregues Lion",
+  "Loving Dolphin",
+  "Optimistic Hawk",
+];
 
-    const spiritAnimal = [
-        "Confident Bear",
-        "Competitive Wolf",
-        "Introverted Panda",
-        "Graceful Butterfly",
-        "Powerful Horse",
-        "Mischivous Fox",
-        "Strongwilled Tiger",
-        "Couregues Lion",
-        "Loving Dolphin",
-        "Optimistic Hawk",
-      ];
-
-      const inputName=document.getElementById("name");
-const input=document.getElementById("input");
-const randomName=function()
-{
-
-    if(inputName==="")
-    {
-        input.innerHTML="Please enter name here";
-    }
-    else
-    {
-        const randomNumber = Math.floor(Math.random()*spiritAnimal.length);
-        const randomSpiritAnimalName = spiritAnimal[randomNumber];
-        input.innerHTML=`${inputName}` + `${randomSpiritAnimalName}`;
-    }
-}
-document.getElementById("click").addEventListener('click', randomName);
+const inputName = document.getElementById("name");
+const input = document.getElementById("input");
+const randomName = function () {
+  if (inputName === "") {
+    input.innerHTML = "Please enter name here";
+  } else {
+    const randomNumber = Math.floor(Math.random() * spiritAnimal.length);
+    const randomSpiritAnimal = spiritAnimal[randomNumber];
+    input.innerHTML = `${inputName}` + `${randomSpiritAnimal}`;
+  }
+};
+document.getElementById("click").addEventListener("click", randomName);
