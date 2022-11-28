99cb624471e48a57c063b48c0345c61ca2b46e88
VioMNE <violeta.pavetic@gmail.com>
2022-11-28 10:09:44 +0100

    corrected files

--- javascript/javascript3/week3/circle.js
+++ javascript/javascript3/week3/circle.js
@@ -1,4 +1,4 @@
-const canvas =  document.getElementById("mycanvas");
+const canvas = document.getElementById("mycanvas");
 const context = canvas.getContext("2d");
 const centerX = canvas.width / 2;
 const centerY = canvas.height / 2;
@@ -38,30 +38,26 @@ class Circle {
 }
 
 const circle1 = new Circle(30, 100, 30, 0, 2 * Math.PI, "red");
-const circle2 = new Circle(170, 100, 30 , 0, 2 * Math.PI, "yellow");
+const circle2 = new Circle(170, 100, 30, 0, 2 * Math.PI, "yellow");
 const circle3 = new Circle(100, 100, 30, 0, 2 * Math.PI, "green");
 
 circle1.draw();
 circle2.draw();
 circle3.draw();
 
-
 setInterval(() => {
-    const purple = Math.floor(Math.random() * 256) + 1;
-    const blue = Math.floor(Math.random() * 256) + 1;
-    const pink = Math.floor(Math.random() * 256) + 1;
-    const maxR = 20;
-    const minR = 3;
+  const purple = Math.floor(Math.random() * 256) + 1;
+  const blue = Math.floor(Math.random() * 256) + 1;
+  const pink = Math.floor(Math.random() * 256) + 1;
+  const maxR = 20;
+  const minR = 3;
 
-    x = Math.random() * canvas.width;
-    y = Math.random() * canvas.height;
-    r = minR + (Math.random() * (maxR - minR));
-    startAngle = 0;
-    endAngle = 2 * Math.PI;
-    fillColor = `rgb ${purple}. ${blue}, ${pink}`;
-    const circle4 = new Circle(x, y, r, startAngle, endAngle, fillColor);
-    circle4.draw()
+  x = Math.random() * canvas.width;
+  y = Math.random() * canvas.height;
+  r = minR + Math.random() * (maxR - minR);
+  startAngle = 0;
+  endAngle = 2 * Math.PI;
+  fillColor = `rgb ${purple}. ${blue}, ${pink}`;
+  const circle4 = new Circle(x, y, r, startAngle, endAngle, fillColor);
+  circle4.draw();
 }, 500);
-
-
-
--- javascript/javascript3/week3/shoppingcart.js
+++ javascript/javascript3/week3/shoppingcart.js
@@ -4,71 +4,76 @@ const username = document.getElementById("user");
 const productPrice = document.getElementById("price");
 
 class Product {
-    constructor(name, price) {
-        this.name = name;
-        this.price = price;
-    }
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
 
-    convertToCurrencyWithApi(currency) {
-        const apiUrl = `https://api.exchangeratesapi.io/latest?base=DKK`;
-        return fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                return this.price * data.rates[currency]
-
-            })
-            .catch(err => console.log(err));
-    }
+  convertToCurrencyWithApi(currency) {
+    const apiUrl = `https://api.exchangeratesapi.io/latest?base=DKK`;
+    return fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        return this.price * data.rates[currency];
+      })
+      .catch((err) => console.log(err));
+  }
 }
 
 class ShoppingCart {
-    constructor() {
-        this.products = [];
-    }
+  constructor() {
+    this.products = [];
+  }
 
-    addProduct(product) {
-        this.products.push(product);
-    }
+  addProduct(product) {
+    this.products.push(product);
+  }
 
-    removeProduct(product) {
-        const newProducts = this.products.filter((existingProduct) => {
-            return existingProduct.name !== product.name
-        })
-        this.products = newProducts;
-    }
+  removeProduct(product) {
+    const newProducts = this.products.filter((existingProduct) => {
+      return existingProduct.name !== product.name;
+    });
+    this.products = newProducts;
+  }
 
-    searchProduct(productName) {
-        const searchedProduct = this.products.filter(product => product.name.toLowerCase() === productName.toLowerCase());
-        return searchedProduct;
-    }
+  searchProduct(productName) {
+    const searchedProduct = this.products.filter(
+      (product) => product.name.toLowerCase() === productName.toLowerCase()
+    );
+    return searchedProduct;
+  }
 
-    renderProducts() {
-        this.products.forEach((product) => {
-            const li = document.createElement('li');
-            const liPrice = document.createElement("li");
-            li.textContent = `${product.name}`;
-            liPrice.textContent = `${product.price}`;
-            productUl.appendChild(li);
-            productPrice.appendChild(liPrice);
-        })
-        return products;
-    }
+  renderProducts() {
+    this.products.forEach((product) => {
+      const li = document.createElement("li");
+      const liPrice = document.createElement("li");
+      li.textContent = `${product.name}`;
+      liPrice.textContent = `${product.price}`;
+      productUl.appendChild(li);
+      productPrice.appendChild(liPrice);
+    });
+    return products;
+  }
 
-    getTotal() {
-        const totalAmount = this.products.map(product => product.price).reduce((acc, currentvalue) => acc + currentvalue);
-        totalPrice.textContent = `The total amount is: ${totalAmount} kr`;
-        console.log(totalAmount);
-    }
+  getTotal() {
+    const totalAmount = this.products.reduce(
+      (currentTotal, product) => currentTotal + product.price,
+      0
+    );
+    totalPrice.textContent = `The total amount is: ${totalAmount} kr`;
+    console.log(totalAmount);
+  }
 
-    getUser() {
-        const url = 'https://jsonplaceholder.typicode.com/users/1'
-        fetch(url)
-            .then(response => response.json())
-            .then(result => {
-                const user = result.username;
-                username.textContent = `Username : ${user}`;
-            })
-    }
+  getUser() {
+    let randomNumber = Math.floor(Math.random() * 10) + 1;
+    const url = `https://jsonplaceholder.typicode.com/users/${randomNumber}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        const user = result.username;
+        username.textContent = `Username : ${user}`;
+      });
+  }
 }
 
 const shoppingCart = new ShoppingCart();
@@ -76,7 +81,7 @@ const flatscreen = new Product("flat-screen", 5000);
 const laptop = new Product("lap-top", 10000);
 const mobile = new Product("mobile-phone", 7000);
 const headphone = new Product("head-phones", 500);
-const officeTable = new Product("office-table", 1500);  
+const officeTable = new Product("office-table", 1500);
 const keybord = new Product("key-bord", 700);
 const mouse = new Product("mouse", 200);
 const officeChair = new Product("office-chair", 2000);
@@ -96,9 +101,9 @@ console.log(shoppingCart);
 console.log(shoppingCart.searchProduct("x-box"));
 
 shoppingCart.getUser();
-const finalCart = shoppingCart.renderProducts()
+const finalCart = shoppingCart.renderProducts();
 
 shoppingCart.getTotal();
-const plant = new Product('plant', 50);
+const plant = new Product("plant", 50);
 shoppingCart.addProduct(plant);
-plant.convertToCurrencyWithApi('USD').then(value => console.log(value));
+plant.convertToCurrencyWithApi("USD").then((value) => console.log(value));
