4399643d186d237434762df0cb51669aa57ae253
VioMNE <violeta.pavetic@gmail.com>
2022-11-28 11:14:37 +0100

    flies has been formated

--- javascript/javascript2/week3/fastestpresser.js
+++ javascript/javascript2/week3/fastestpresser.js
@@ -11,28 +11,27 @@ let gameEnd = false;
 let gameStart = false;
 
 function keyDownCounter() {
-    if (input.value > 0 && !gameEnd && gameStart) {if (event.code === "KeyS") {
-        counterS++;
-        countS.textContent = counterS;
+  if (input.value > 0 && !gameEnd && gameStart) {
+    if (event.code === "KeyS") {
+      counterS++;
+      countS.textContent = counterS;
+    } else if (event.code === "KeyL") {
+      counterL++;
+      countL.textContent = counterL;
+    } else {
+      alert(`Please press either "S" or "L"`);
     }
-    else if (event.code === "KeyL") {
-        counterL++;
-        countL.textContent = counterL;
-    }
-    else {
-        alert(`Please press either "S" or "L"`);
-    }
+  }
 }
-};
 
 function startGame() {
-gameStart = true;
-if (input.value === "") {
+  gameStart = true;
+  if (input.value === "") {
     alert("enter number of seconds you want to play!");
     return;
-}
+  }
 
-if (gameEnd) {
+  if (gameEnd) {
     messageForS.textContent = "";
     messageForL.textContent = "";
     counterS = 0;
@@ -42,37 +41,36 @@ if (gameEnd) {
     clearConfetti();
     gameEnd = false;
     gameStart = true;
-}
-setTimeout(() => {
+  }
+  setTimeout(() => {
     displayGameResult();
-}, input.value * 1000);
+  }, input.value * 1000);
 }
 
 function displayGameResult() {
-gameEnd = true;
-button.innerText = "Play Again!"; 
-if (counterS > counterL) { messageForS.textContent = "Congratulations! You are the winner!.";
-renderConfetti('s-box');
-}
-else if (counterS < counterL) {
-messageForL.textContent = "Congratulations! You are the winner!";
-renderConfetti('l-box');
-}
-else if (counterS = counterL) {
-messageForS.textContent = "Sorry, No winner this time";
-messageForL.textContent = "Sorry, No winner this time";
-}
+  gameEnd = true;
+  button.innerText = "Play Again!";
+  if (counterS > counterL) {
+    messageForS.textContent = "Congratulations! You are the winner!.";
+    renderConfetti("s-box");
+  } else if (counterS < counterL) {
+    messageForL.textContent = "Congratulations! You are the winner!";
+    renderConfetti("l-box");
+  } else if ((counterS = counterL)) {
+    messageForS.textContent = "Sorry, No winner this time";
+    messageForL.textContent = "Sorry, No winner this time";
+  }
 }
 function renderConfetti(targetId) {
-let confettiSettings = { target: targetId, width: 400, height: 200 };
-const confetti = new confettiGenerator(confettiSettings);
-confetti.render();
-confettiGenerator = confetti;
+  let confettiSettings = { target: targetId, width: 400, height: 200 };
+  const confetti = new confettiGenerator(confettiSettings);
+  confetti.render();
+  confettiGenerator = confetti;
 }
 
 function clearConfetti() {
-confettiGenerator.clear();
+  confettiGenerator.clear();
 }
 
-document.addEventListener('keydown', keyDownCounter);
+document.addEventListener("keydown", keyDownCounter);
 button.addEventListener("click", startGame);
