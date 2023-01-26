2bdab750718523ad945025845b8de712946e5a2b
VioMNE <violeta.pavetic@gmail.com>
2023-01-26 12:48:47 +0100

    added files form db homework week2

---
+++ databases/week2/part2.sql
@@ -0,0 +1,39 @@
+-- Part 2: School database
+-- Create a new database containing the following tables:
+
+CREATE DATABASE school;
+
+USE school;
+
+SET NAMES utf8mb4;
+
+-- Class: with the columns: id, name, begins (date), ends (date)
+CREATE TABLE `Class` (
+`id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(225) NOT NULL,
+`begins_date` DATETIME NOT NULL,
+`end_date` DATETIME NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Student: with the columns: id, name, email, phone, class_id (foreign key)
+
+CREATE TABLE `Student` (
+`id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(50) NOT NULL,
+`email` VARCHAR(15) NOT NULL,
+`phone` VARCHAR(15) NOT NULL,
+`class_id`VARCHAR(10) NOT NULL,
+CONSTRAINT `fk_ckass` FOREIGN KEY (`class_id`) REFERENCES `class`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Create an index on the name column of the student table.
+
+ALTER TABLE Student ADD INDEX inx_newindex (name);
+
+-- Add a new column to the class table named status which can only have the following values: not-started, ongoing, finished
+
+ALTER TABLE Class
+ ADD status ENUM(
+ 'not-started', 
+ 'ongoing', 
+ 'finished');
---
+++ databases/week2/part3.sql
@@ -0,0 +1,38 @@
+USE hyf_lesson2;
+
+-- Part 3: More queries
+
+-- 1. Get all the tasks assigned to users whose email ends in @spotify.com
+
+SELECT task.title AS 'Task Name',
+user.email as 'email'
+FROM user_task
+JOIN task ON user_task.task_id = task.id
+JOIN user ON user_task.user_id = user.id
+WHERE user.email LIKE '%@spotify.com';
+
+-- 2. Get all the tasks for 'Donald Duck' with status 'Not started'
+
+SELECT task.id, task.titile, task.created, user.name 
+FROM task
+JOIN user_task ON task.id=user_task.task_id
+JOIN user ON user_task.user_id = user.id
+WHERE status.name = 'Not Started' AND user.name= 'Donald Duck';
+
+-- 3.Get all the tasks for 'Maryrose Meadows' that were created in september
+
+SELECT 
+task.title AS  'Task Name',
+user.name AS 'user',
+task.created as created
+FROM user_task
+JOIN task ON user_task.task_id = task.id
+JOIN user ON user_task.user_id = user.id
+WHERE MONTH(created) = 09 AND user.name = 'Maryrose Meadows';
+
+
+-- 4.Find how many tasks where created in each month
+
+SELECT month(created), COUNT(*)
+FROM task
+GROUP BY month(created);
